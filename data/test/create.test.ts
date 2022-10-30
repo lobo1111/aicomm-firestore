4e0ae0169ba6c4d5c292afbfb7ad3cda3240cf0d
Tomasz Kopacki <tomasz@kopacki.eu>
2022-10-30 09:12:24 +0100

    initial commit

---
+++ data/test/create.test.ts
@@ -0,0 +1,64 @@
+import * as fs from 'fs'
+import { v4 } from "uuid"
+import * as firebase from '@firebase/rules-unit-testing'
+
+const projectID = v4()
+let testEnv: firebase.RulesTestEnvironment
+
+beforeAll(async () => {
+  testEnv = await firebase.initializeTestEnvironment({
+    projectId: projectID,
+    firestore: {
+      rules: fs.readFileSync('./firestore.rules', 'utf8')
+    }
+  })
+})
+
+beforeEach(async () => {
+  await testEnv.clearFirestore()
+})
+
+afterAll(async () => {
+  await testEnv.cleanup()
+})
+
+describe('create user profile', () => {
+    it('create user profile as HR department member', async () => {
+    const uid = v4()
+    const context = testEnv.authenticatedContext(uid, {claim: "HR"})
+    const profile = {
+        "email": "test@test.com",
+        "manager": "manager@test.com",
+        "department": "engineering"
+    }
+    await firebase.assertSucceeds(
+      context.firestore().doc("userProfile/" + uid).set(profile)
+    )
+  })
+
+  it('create user profile as non HR department member', async () => {
+    const uid = v4()
+    const context = testEnv.authenticatedContext(uid)
+    const profile = {
+        "email": "test@test.com",
+        "manager": "manager@test.com",
+        "department": "engineering"
+    }
+    await firebase.assertFails(
+      context.firestore().doc("userProfile/" + uid).set(profile)
+    )
+  })
+
+  it('create user profile as unauthenticated user', async () => {
+    const uid = v4()
+    const context = testEnv.unauthenticatedContext()
+    const profile = {
+        "email": "test@test.com",
+        "manager": "manager@test.com",
+        "department": "engineering"
+    }
+    await firebase.assertFails(
+      context.firestore().doc("userProfile/" + uid).set(profile)
+    )
+  }) 
+})
---
+++ data/test/update.test.ts
@@ -0,0 +1,41 @@
+import * as fs from 'fs'
+import { v4 } from "uuid"
+import * as firebase from '@firebase/rules-unit-testing'
+
+const projectID = v4()
+let testEnv: firebase.RulesTestEnvironment
+
+beforeAll(async () => {
+  testEnv = await firebase.initializeTestEnvironment({
+    projectId: projectID,
+    firestore: {
+      rules: fs.readFileSync('./firestore.rules', 'utf8')
+    }
+  })
+})
+
+beforeEach(async () => {
+  await testEnv.clearFirestore()
+})
+
+afterAll(async () => {
+  await testEnv.cleanup()
+})
+
+describe('update user profile', () => {
+  it('update user profile as HR department member', async () => {
+    const uid = v4()
+    const hr = testEnv.authenticatedContext(uid, {claim: "HR"})
+    const profile = {
+        "email": "test@test.com",
+        "manager": "other-manager@test.com",
+        "department": "engineering"
+    }
+    const hrContext = hr.firestore()
+     await firebase.assertSucceeds(
+      hrContext.doc("userProfile/" + uid).set(profile).then(_ => {
+        hrContext.doc("userProfile/" + uid).update(profile)
+      })
+    ) 
+  })
+})
